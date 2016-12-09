4bddc1699fc6746e5378f307d351a490bc320186
Mos Roshanavand <roshanavand@gmail.com>
2016-12-09 19:00:28 +0200

    Add webserver configuration
    - Add local network in kitchen config for manual and selenium tests
    - Fix generating secret token for the app
    - Change default user
    - Change cookbook version

--- cookbooks/hello_world/recipes/default.rb
+++ cookbooks/hello_world/recipes/default.rb
@@ -2,39 +2,59 @@
 # Cookbook Name:: hello_world
 # Recipe:: default
 #
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Copyright (c) 2016 Mos Roshanavand, All Rights Reserved.
 
 include_recipe 'apt'
 include_recipe 'build-essential'
+
+apt_repository 'phusionpassenger' do
+  uri node[:passenger_repo_uri]
+  distribution 'xenial'
+  components ['main']
+  key node[:passenger_repo_key]
+  keyserver node[:passenger_repo_keyserver]
+  action :add
+end
 
 %w(net-tools ruby-dev zlib1g-dev libsqlite3-dev libmysqlclient-dev nodejs).each do |pkg|
   package pkg
 end
 
-ruby_runtime node[:app_name] do
-  provider :system
-  version node[:ruby_version]
-end
-
 application node[:app_dir] do
   owner node[:user]
   group node[:user]
+  ruby node[:ruby_version]
   git node[:app_repo]
 
   bundle_install do
+    user node[:user]
     deployment true
     without node[:ignored_env]
   end
 
   rails do
     rails_env node[:app_env]
+    secrets_mode :yaml
+    secret_token node[:app_secret_token]
     database({
       adapter: node[:db_type],
       host: node[:db_host],
       username: node[:db_user],
       password: node[:db_pass],
-      database: node[:app_name],
+      database: node[:db_name],
     })
     migrate true
   end
+end
+
+include_recipe 'chef_nginx::default'
+include_recipe 'chef_nginx::passenger'
+
+nginx_site 'default' do
+  action :disable
+end
+
+nginx_site node[:app_name] do
+  template 'hello_world.cnf.erb'
+  notifies :restart, 'service[nginx]'
 end
